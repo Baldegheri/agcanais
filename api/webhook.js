07d8e9d069877aeb488382d6dca0a1bc138c3082
Baldegheri <bruno.bernardes@agcapital.com.br>
2025-03-19 20:59:41 -0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,2 +1,9 @@
-api/webhook.js
+export default function handler(req, res) {
+    if (req.method === "POST") {
+        console.log("Recebendo evento do HubSpot:", req.body);
+        res.status(200).json({ message: "Webhook recebido com sucesso!" });
+    } else {
+        res.status(405).json({ message: "Método não permitido" });
+    }
+}
 
